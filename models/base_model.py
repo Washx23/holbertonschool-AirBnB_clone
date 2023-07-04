5feb80ea94646ed3c804ae8cf3b0b075a41e2a24
Washx23 <5731@holbertonstudents.com>
2023-07-04 14:51:35 -0700

    fix errors 0.1

--- models/base_model.py
+++ models/base_model.py
@@ -22,29 +22,29 @@ class BaseModel:
                 elif key == 'created_at':
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if 'id' not in kwargs.keys():
-                    self.id = str(uuid4())
+                    self.id = str(uuid.uuid4())
                 if 'created_at' not in kwargs.keys():
                     self.created_at = datetime.now()
                 if 'updated_at' not in kwargs.keys():
                     self.updated_at = datetime.now()
                 setattr(self, key, value)
-        
+
     def update(self):
+        "document"
         self.updated_at = datetime.now()
-    
+
+    def __str__(self):
+        "document"
+        return (f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
+
     def save(self):
-        self.update()
-        
+        "document"
+        self.updated_at = datetime.now()
+
     def to_dict(self):
+        """document"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-    
-    def __str__(self):
-        class_name = self.__class__.__name__
-        obj_id = self.id
-        obj_dict = self.__dict__
-        return "[{}] ({}) {}".format(class_name, obj_id, obj_dict)
-    
